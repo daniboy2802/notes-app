caa597f0509b0de052d0223498da767b1c31a9c5
Daniel <dv.santibanez@gmail.com>
2021-10-19 11:18:14 -0500

    we did delete semantic ui react from the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../src/styles/global.scss'
-import 'semantic-ui-css/semantic.min.css'
 import Layout from '../src/components/layout'
 
 // This default export is required in a new `pages/_app.js` file.
--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import styles from '../styles/modules/header.module.scss'
-import { Container, Icon } from 'semantic-ui-react'
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <container className={styles['main-container']}>
         <div className={styles['title-container']}>
-          <span>
-            <Icon 
-              size='big'
-              color='orange'
-              name='sticky note outline'
-            />
-          </span>
           <span>
             <p>NotesApp</p>
           </span>
